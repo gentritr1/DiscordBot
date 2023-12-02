5bf350b66b5a0b4f2db934538e814f60bb3fc2a2
Gentrit Rashiti <gentrit@vianova.ai>
2023-12-02 21:45:59 +0100

    db service as a single source of truth.

---
+++ src/services/dbService.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+class DBService {
+  constructor(connectionString) {
+    this.connectionString = connectionString;
+    this.connected = false;
+  }
+
+  async connect() {
+    if (!this.connected) {
+      await mongoose.connect(this.connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      this.connected = true;
+    }
+  }
+}
+
+const dbService = new DBService(process.env.MONGO_DB_URI);
+
+module.exports = dbService; // Export the initialized instance
