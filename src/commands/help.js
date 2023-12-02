14638220b5bc2069ece4f016742973899bd2ab41
Gentrit Rashiti <gentrit@vianova.ai>
2023-12-02 22:53:26 +0100

    randomized send of message.

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,9 @@
+const { sendRandomizedResponse } = require("../utils/responseUtils");
+
 const execute = (message) => {
   const helpMessage = `Available Commands:\n!study start - Start a study session\n!study stop - Stop a study session\n!help - Show this message`;
-  message.channel.send(helpMessage);
+
+  sendRandomizedResponse(message, helpMessage);
 };
 
 module.exports = {
