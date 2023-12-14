0e7a07e051786c94c9472839823223c215eea72b
Gentrit Rashiti <gentrit@vianova.ai>
2023-12-14 23:32:04 +0100

    snowmanager has new methods for the snowball game.

--- src/utils/snowManager.js
+++ src/utils/snowManager.js
@@ -1,3 +1,6 @@
+const SnowballStats = require("../models/SnowballStats");
+const SnowballService = require("../services/SnowballService");
+
 let snowInterval = null;
 let awaitingResponse = false;
 let responseCheckInterval = null;
@@ -133,6 +136,135 @@ const checkUserResponse = (message) => {
     });
 };
 
+let snowballFightParticipants = new Map();
+let inactivityWarnings = new Map();
+
+const soloTargets = [
+  "Snowman",
+  "Ice Sculpture",
+  "Frozen Tree",
+  "Roaming Penguin",
+];
+
+const warnInactivePlayer = (userId, username, channel) => {
+  if (inactivityWarnings.has(userId)) {
+    channel.send(
+      `${username}, you have been removed from the snowball fight due to inactivity.`
+    );
+    leaveSnowballFight({ id: userId, username: username }, channel);
+    inactivityWarnings.delete(userId);
+  } else {
+    channel.send(
+      `${username}, please throw a snowball or you'll be removed for inactivity.`
+    );
+    inactivityWarnings.set(
+      userId,
+      setTimeout(() => {
+        warnInactivePlayer(userId, username, channel);
+      }, 60000)
+    ); // 60 seconds
+  }
+};
+
+const throwSnowballSolo = async (message, thrower) => {
+  console.log("Throwing solo?", thrower, message);
+  const target = soloTargets[Math.floor(Math.random() * soloTargets.length)];
+  const hit = Math.random() < 0.5; // Simple hit/miss logic for solo play
+
+  if (hit) {
+    message.channel.send(`🎯 ${thrower.username} hit the ${target}!`);
+  } else {
+    message.channel.send(`❌ ${thrower.username} missed the ${target}!`);
+  }
+
+  // Reset inactivity timer since the player is active
+  clearTimeout(inactivityWarnings.get(thrower.id));
+  inactivityWarnings.delete(thrower.id);
+
+  // Restart inactivity timer for next throw
+  inactivityWarnings.set(
+    thrower.id,
+    setTimeout(() => {
+      warnInactivePlayer(thrower.id, thrower.username, message.channel);
+    }, 120000)
+  ); // 120 seconds
+};
+
+const joinSnowballFight = async (message, user) => {
+  const isNewUser = await SnowballService.joinSnowballFight(
+    user.id,
+    user.username
+  );
+  if (!isNewUser) {
+    message.reply(`${user.username} is already in the snowball fight!`);
+    return;
+  }
+  snowballFightParticipants.set(user.id, { username: user.username }); // Use user.id as key
+  message.channel.send(`${user.username} has joined the snowball fight! ❄️🌨️`);
+};
+
+const leaveSnowballFight = async (user, channel) => {
+  const leftSuccessfully = await SnowballService.leaveSnowballFight(user.id);
+  if (!leftSuccessfully) {
+    channel.send(`${user.username} is not in the snowball fight.`);
+    return;
+  }
+  snowballFightParticipants.delete(user.id);
+  channel.send(`${user.username} has left the snowball fight.`);
+  if (snowballFightParticipants.size < 2) {
+    snowballFightParticipants.clear();
+    channel.send("Snowball fight has ended due to lack of participants.");
+  }
+};
+
+const throwSnowball = async (message, thrower, targetUsername) => {
+  // Reset inactivity timer
+  clearTimeout(inactivityWarnings.get(thrower.id));
+  inactivityWarnings.delete(thrower.id);
+
+  if (!snowballFightParticipants.has(thrower.id)) {
+    // Check using thrower.id
+    message.reply("You must join the snowball fight first!");
+    return;
+  }
+
+  if (snowballFightParticipants.size <= 1) {
+    console.log("here since only 1 player", message, thrower);
+    await throwSnowballSolo(message, thrower); // Handle solo play
+    return;
+  }
+
+  let target = [...snowballFightParticipants.values()].find(
+    (u) => u.username === targetUsername
+  );
+  if (!target) {
+    message.reply(`Could not find ${targetUsername} in the snowball fight.`);
+    return;
+  }
+
+  const hit = Math.random() < 0.5; // Simple hit/miss logic
+  if (hit) {
+    target.hits++;
+    message.channel.send(
+      `💥 ${thrower.username} hit ${target.username} with a snowball!`
+    );
+    await SnowballService.recordThrow(thrower.id, true);
+  } else {
+    message.channel.send(
+      `❌ ${thrower.username} missed ${target.username} with a snowball!`
+    );
+    await SnowballService.recordThrow(thrower.id, false);
+  }
+
+  // Start inactivity timer
+  inactivityWarnings.set(
+    thrower.id,
+    setTimeout(() => {
+      warnInactivePlayer(thrower.id, thrower.username, message.channel);
+    }, 120000)
+  ); // 120 seconds
+};
+
 module.exports = {
   startSnow,
   stopSnow,
@@ -140,4 +272,7 @@ module.exports = {
   checkUserResponse,
   updateSnowIntensity,
   updateSnowTheme,
+  throwSnowball,
+  leaveSnowballFight,
+  joinSnowballFight,
 };
