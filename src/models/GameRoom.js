916bd444c278851cc8a9c9177a49df66f3b34493
Gentrit Rashiti <gentrit@vianova.ai>
2023-12-15 15:47:16 +0100

    gameroom schema model in mongo db.

---
+++ src/models/GameRoom.js
@@ -0,0 +1,11 @@
+// models/GameRoom.js
+const mongoose = require("mongoose");
+
+const gameRoomSchema = new mongoose.Schema({
+  roomId: { type: String, required: true, unique: true },
+  participants: [{ userId: String, username: String }],
+  createdAt: { type: Date, default: Date.now },
+  currentRound: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model("GameRoom", gameRoomSchema);
