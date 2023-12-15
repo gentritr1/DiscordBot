c6bd3d916df2b517fe2fdd528822df1579ae41f2
Gentrit Rashiti <gentrit@vianova.ai>
2023-12-15 20:01:06 +0100

    refactor in game room model.

--- src/models/GameRoom.js
+++ src/models/GameRoom.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 
 const gameRoomSchema = new mongoose.Schema({
   roomId: { type: String, required: true, unique: true },
-  participants: [{ userId: String, username: String }],
+  participants: [
+    {
+      userId: String,
+      username: String,
+    },
+  ],
   createdAt: { type: Date, default: Date.now },
   currentRound: { type: Number, default: 0 },
 });
